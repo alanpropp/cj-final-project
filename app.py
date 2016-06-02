23d99d52dee4ae2907633a715b07fdfc61dc6c43
alanpropp <apropp@stanford.edu>
2016-06-01 20:58:36 -0700

    Tryna 2 chart it

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request
 #from foo import just_do_it
 import helpers
-from collections import OrderedDict
 app = Flask(__name__)
 
 @app.route("/")
@@ -14,8 +13,10 @@ def homepage():
 @app.route("/results")
 def results():
     reqargs = request.args
+    
     #First, we need to know what counry we are talking about
     country = reqargs.get('country')
+
     #Throw an error if there's no country entered
     if not country:
         return """
@@ -27,19 +28,57 @@ def results():
     #Next, what metric are they requesting
     metric = reqargs.get('metric')
 
-    information = []
-    #Fill 'electricity' with this specific country's info
-    if reqargs.get('metric') == 'coal':
-        information = helpers.coal()
-    
+    #Get the information for the metric requseted
+    information = helpers.get_correct_metric(metric)
+    years_list = []
     argument = []
+
+    #Get a list of the nation's usage over the years, put it in 'arguments' list
+    #Arguments is a list of floats that corresponds to each spot in years_list
     for i in information:
         if i[''] == country:
             for y in helpers.years():
-                argument.append(i[y])
+                if(i.get(y)):
+                    if(i.get(y) != 'NA' and i.get(y) != '--'):
+                        argument.append(float(i[y]))
+                        years_list.append(y)
+                    else:
+                        argument.append(None)
+                        years_list.append(y)
 
 
-    return render_template('results.html', nation = country, argument = argument, years = helpers.years())
+
+    units = reqargs.get('units')
+    population = []
+
+    #Get the units of whatever we are doing
+    metric_units = helpers.get_units(metric)
+    
+    metric_units = metric_units + " per capita"
+    population_overall = helpers.population()
+    for p in population_overall:
+        if p[''] == country:
+            for y in years_list:
+                if(p[y]):
+                    if p[y] != '--' and p[y] != 'NA':
+                        population.append(float(p[y]))
+                    else:
+                        population.append(None)
+
+    if units == 'per capita':
+        for a in range(0,len(argument)):
+            if population[a] != None and argument[a] != None:
+                b = float(argument[a])/float(population[a])
+                argument[a] = b
+            else:
+                argument[a] = None
+
+    #Time to find similar countries!
+    similar_countries = helpers.find_similar_countries(information, years_list, argument, country)
+    percentage = helpers.percentage_total(information, years_list, argument)
+    return render_template('results.html', nation = country, argument = argument, years = years_list,
+        units = units, metric = metric, metric_units = metric_units, similar = similar_countries,
+        percentage = percentage, population = population)
 
 
 if __name__ == '__main__':
--- helpers.py
+++ helpers.py
@@ -1,7 +1,6 @@
 from operator import itemgetter
 import csv
 from os.path import join
-from collections import OrderedDict
 BASE = 'Data'
 
 def get_data(filename):
@@ -27,18 +26,85 @@ def years():
             years_list.append(line[0:4])
     return years_list
 
-def get_Ordered_Dict(diction):
-    years_list = years()
-    product = OrderedDict()
-    for year in years_list:
-        product[year] = diction[year]
-    return product
+def get_correct_metric(metric):
+    information = []
+    if metric == 'coal':
+        information = coal()
+    elif metric == 'electricity':
+        information = electricity_generation()
+    elif metric == 'natural gas':
+        information = natural_gas()
+    elif metric == 'biofuels':
+        information = biofuels()
+    elif metric == 'energy':
+        information = primary_energy_consumption()
+    elif metric == 'renewable electricity':
+        information = renewable_electricity_consumption()
+    return information
+
+def get_units(metric):
+    units = ""
+    if metric == 'coal':
+        units = "quadrillion Btu"
+    elif metric == 'electricity':
+        units = 'billion kilowatt-hours'
+    elif metric == 'natural gas':
+        units = 'quadrillion Btu'
+    elif metric == 'biofuels':
+        units = 'thousand barrels per day'
+    elif metric == 'energy':
+        units = 'quadrillion Btu'
+    elif metric == 'renewable electricity':
+        units = 'billion kilowatt-hours'
+    return units
+
+
+def find_similar_countries(information, years_list, argument, country):
+    similar_countries = [] #List of countries along with their relevant data points
+    relevant_year = ""
+    counter = len(argument)-1
+    while counter>0:
+        if argument[counter]!=None:
+            relevant_year = years_list[counter]
+            break
+        counter-=1
+    if counter!=0:
+        country_data_point = float(argument[counter])
+        for i in information:
+            curr_country = i['']
+            data_point = i[relevant_year]
+            if data_point != 'NA' and data_point != '--' and curr_country != country:
+                absolute = abs(country_data_point-float(data_point))
+                entry = []
+                entry.append(curr_country)
+                entry.append(absolute)
+                entry.append(float(data_point))
+                similar_countries.append(entry)
+            similar_countries = sorted(similar_countries, key = itemgetter(1))
+    return similar_countries[0:10]
+
+def percentage_total(information, years_list, argument):
+    counter = len(argument)-1
+    relevant_year = ""
+    world_total = ""
+    percentage = 0
+    while counter>0:
+        if argument[counter]!=None:
+            relevant_year = years_list[counter]
+            break
+
+    for i in information:
+        if i['']=='World':
+            world_total = i[relevant_year]
+            percentage = 100*float(argument[counter])/float(world_total)
+            break
+    return percentage
+
 
 
 def natural_gas():
     FNAME = 'drynaturalgasconsumption.csv'
     return get_data(filename = FNAME)
-
 
 def biofuels():
     FNAME = 'totalbiofuelsproduction.csv'
